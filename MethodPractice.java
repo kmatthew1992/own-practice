35961e58f8d83177fdf8a83d376b64ddf449efa6
kmatthew1992 <koszegim8@gmail.com>
2024-03-04 11:40:54 +0100

    Method first practice

---
+++ MethodPractice.java
@@ -0,0 +1,28 @@
+public class MethodPractice {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5 + 5;
+        int c = a + b;
+        System.out.println("első: " + c);
+        System.out.print("második: ");
+        sum1(a, b);
+        System.out.print("harmadik: ");
+        sum1(b, c);
+        System.out.print("negyedik: ");
+        sum2(a, b, c);
+        System.out.println("ötödik: " + sum3());
+    }
+
+    private static void sum1 ( int a, int b){
+        int sum = a + b;
+        System.out.println(sum);
+    }
+    private static void sum2(int a, int b, int c){
+        int sum = a + b +c;
+        System.out.println(sum);
+    }
+    private static int sum3() {return 15 + 12;
+    }
+}
